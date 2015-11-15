a653cc68a53216fe94c191c7b1e44fcd6427fe90
Dennise Saxton <dennisepnd@gmail.com>
2015-11-15 15:39:06 -0600

    Added show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+  end
 end
