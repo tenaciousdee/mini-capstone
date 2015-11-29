7a95bece3039279ec4d76b6707979d64d79ded8a
Dennise Saxton <dennisepnd@gmail.com>
2015-11-29 10:24:45 -0600

    Changed price to decimal migration for postgres

--- db/migrate/20151119031707_change_price_type_to_products.rb
+++ db/migrate/20151119031707_change_price_type_to_products.rb
@@ -1,5 +1,5 @@
 class ChangePriceTypeToProducts < ActiveRecord::Migration
   def change
-    change_column :products, :price, :decimal, precision: 6, scale: 2
+    change_column :products, :price, 'decimal USING CAST(price AS decimal)'
   end
 end
