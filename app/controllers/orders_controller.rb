fe8362801fb9eee4c1c15594e9ed00f906c7175f
Dennise Saxton <dennisepnd@gmail.com>
2015-12-01 19:09:56 -0600

    Removed all logic involving product id and quantity from order controller and order show view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,6 @@
 class OrdersController < ApplicationController
   def create
     order = Order.create(
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       user_id: current_user.id)
 
     flash[:success] = "Order was successfully created"
